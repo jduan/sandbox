068142c2762a3633a0c5d0e6b58b6fb5e6e23b4c
Jingjing Duan <jduan@twitter.com>
2014-02-19 15:09:35 -0800

    remove MySQL stuff

--- birdcage_queue.sh
+++ birdcage_queue.sh
@@ -2,17 +2,6 @@
 
 export NUM_OF_SHARDS=6
 
-function create_database() {
-  envvar=$1
-  prefix=$2
-
-  dbname=$(echo "$prefix-$(uuidgen)" | tr '-' _)
-  echo CREATE DATABASE IF NOT EXISTS $dbname | mysql -u root -S $MYSQL_UNIX_PORT
-  trap "echo DROP DATABASE $dbname | mysql -u root -S $MYSQL_UNIX_PORT" EXIT
-
-  export $envvar=$dbname
-}
-
 if [[ ${build:-0} == 'maven' ]]; then
   export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=256m"
   find ~/.m2 -name *SNAPSHOT -type d | xargs rm -rf
@@ -25,15 +14,5 @@ else
   export SUBMIT_QUEUE_MODE=1
   export AUTO_CI_FAIL_SLOW=1
 
-  source /data/jenkins/bin/manage_mysql.sh clean
-
-  /data/jenkins/bin/manage_mysql.sh start
-  trap "/data/jenkins/bin/manage_mysql.sh stop" EXIT
-
-  # querulous/querulous-core test support
-  export DB_HOST="localhost:$MYSQL_TCP_PORT"
-  create_database DB_NAME querulous
-
   ./pants-support/jenkins/scripts/auto-ci-jvm.sh
 fi;
-
